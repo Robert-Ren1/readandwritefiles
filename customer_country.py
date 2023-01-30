9f2b776c90494c9e14160162499159b2bd06b747
Robert-Ren1 <rzq916@hotmail.com>
2023-01-29 19:19:35 -0600

    Homework

--- customer_country.py
+++ customer_country.py
@@ -5,8 +5,9 @@ out_file = open("customer_country.csv", 'w')
 reader = csv.reader(customer_file, delimiter=",")
 
 next(reader)
-
+out_file.write("FirstName"+','+'LastName'+','+'Country'+'\n')
 for i in reader:
-    out_file.write(i[1]+', '+i[2]+', '+i[4]+'\n')
+    out_file.write(i[1]+','+i[2]+','+i[4]+'\n')
 
+customer_file.close()
 out_file.close()
--- customer_sales_report.py
+++ customer_sales_report.py
@@ -3,12 +3,25 @@ infile = open('sales.csv', 'r')
 outfile = open("salesreport.csv", 'w')
 
 read = csv.reader(infile, delimiter=',')
-
 next(read)
-outfile.write('CustomerID'+', ' + "calculated total" + '\n')
+outfile.write('CustomerID'+' | ' + "Total" + '\n')
+value = 250
+total = 0
 for i in read:
-    total = (float(i[3])+float(i[4])+float(i[5]))
-    total = str(total)
-    outfile.write(i[0]+', '+total+"\n")
+    if int(i[0]) == value:
+        total = (total + float(i[3])+float(i[4])+float(i[5]))
+    elif int(i[0]) != value:
+        result = "{:.2f}".format(total)
+        outfile.write("       "+str(value) + '   ' + str(result)+'\n')
+        value += 1
+        total = 0
+        total += float(i[3])+float(i[4])+float(i[5])
+
+try:
+    next(read)
+except StopIteration:
+    result = "{:.2f}".format(total)
+    outfile.write("       "+str(value) + '   ' + str(result)+'\n')
+
 
 outfile.close()
